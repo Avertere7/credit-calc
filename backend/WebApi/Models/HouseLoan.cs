554b2acec340457a1caf2744f2125b3d91fb9aeb
Avertere7 <avertere1996@gmail.com>
2021-02-24 00:24:06 +0100

    init models and controllers

---
+++ backend/WebApi/Models/HouseLoan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Interfaces;
+
+namespace WebApi.Models
+{
+    public class HouseLoan : ILoan
+    {
+        public double LoanAmount { get; set; }
+        public int RepaymentPeriod { get; set; }
+        public double InterestRate { get; set; }
+
+        public LoanSchedule CreateLoanSchedule()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
